0fb9a479403f29cf612d13bd32ee90b0cf34e51d
Martin McDonald <your.neogen.email@neogen.com>
2025-09-30 12:49:09 +0100

    fix(iv-guide): overwrite with clean header to clear IndentationError

--- pages/03_Interview_Guide_Generator.py
+++ pages/03_Interview_Guide_Generator.py
@@ -1,11 +1,8 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import streamlit as st
-from utils.branding import header, sidebar_model_controls, inject_css
-from utils.llm import chat_complete
-from utils.parsers import extract_text_from_upload
-from utils.exporters import interview_pack_to_docx_bytes  # boxed DOCX
+from utils.branding import header, inject_css
 
 st.set_page_config(page_title="Interview Guide Generator", page_icon="🧩")
 header("Interview Guide Generator")
 inject_css()
-st.success("Header loaded cleanly. If you see this, the indentation error is gone.")
+st.success("Header loaded cleanly. If you see this, the indentation issue is gone.")
